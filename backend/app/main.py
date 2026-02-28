5da8f61d90743d6dd210b7cef88de76e78a282cc
Shikhar Mishra <shikharmishra6350650@gmail.com>
2026-02-28 11:18:43 +0530

    update main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,7 @@ from .core.config import settings
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
-# from .routers import resume, evaluate, plan, quiz, interview, jobs, progress, auth
-from .routers import auth
+from .routers import resume, evaluate, plan, quiz, interview, jobs, progress, auth
 
 app: FastAPI = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 app.add_middleware(
@@ -15,11 +14,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# app.include_router(resume.router, prefix="/resume", tags=["resume"])
+app.include_router(resume.router, prefix="/resume", tags=["resume"])
 # app.include_router(evaluate.router, prefix="/evaluate", tags=["evaluate"])
-# app.include_router(plan.router, prefix="/plan", tags=["plan"])
+app.include_router(plan.router, prefix="/plan", tags=["plan"])
 # app.include_router(quiz.router, prefix="/quiz", tags=["quiz"])
-# app.include_router(interview.router, prefix="/interview", tags=["interview"])
+app.include_router(interview.router, prefix="/interview", tags=["interview"])
 # app.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
 # app.include_router(progress.router, prefix="/progress", tags=["progress"])
 app.include_router(auth.router, prefix="/auth", tags=["authentication"])
